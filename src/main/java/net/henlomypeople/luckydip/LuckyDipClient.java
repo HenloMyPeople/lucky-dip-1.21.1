cc6a04f828f63136cd17457ede5ad31132d82149
Sam <samelliot99@outlook.com>
2026-03-17 12:16:34 +0000

    Initial commit

---
+++ src/main/java/net/henlomypeople/luckydip/LuckyDipClient.java
@@ -0,0 +1,10 @@
+package net.henlomypeople.luckydip;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LuckyDipClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
